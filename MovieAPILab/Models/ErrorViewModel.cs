c14225e1bf23ea3cf9c66304a7a2d263241dd1fa
belle <belle.baxley@gmail.com>
2023-02-15 08:49:19 -0500

    first commit

---
+++ MovieAPILab/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace MovieAPILab.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
+
---
+++ MovieAPILab/Models/MovieDAL.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Newtonsoft.Json;
+using System.Net;
+
+namespace MovieAPILab.Models
+{
+	public class MovieDAL
+	{
+        public static MovieModel GetMovie(string searchTerm) //adjust here (output type and method name)
+        {
+            //Setup
+            string url = $"https://www.omdbapi.com/?t={searchTerm}&apikey=7ed57eb1";
+
+            //Request
+            HttpWebRequest request = WebRequest.CreateHttp(url);
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+
+            //Convert it to JSON
+            StreamReader reader = new StreamReader(response.GetResponseStream());
+            string JSON = reader.ReadToEnd();
+
+            //Convert to C#
+            MovieModel result = JsonConvert.DeserializeObject<MovieModel>(JSON);
+            return result;
+            //adjust here (change to match object name)
+        }
+    }
+}
+
